cbb9726f608a4542ad459a3e00d49bb831721ffe
Shailesh Devadiga <sdevadiga@microsoft.com>
2022-08-22 15:27:25 +0530

    Modified for DYMK

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,8 +10,8 @@ import { ProfileComponent } from './examples/profile/profile.component';
 import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 
 const routes: Routes =[
-    { path: '', redirectTo: 'index', pathMatch: 'full' },
-    { path: 'index',                component: ComponentsComponent },
+    { path: '', redirectTo: 'dymk', pathMatch: 'full' },
+    { path: 'dymk',                component: ComponentsComponent },
     { path: 'nucleoicons',          component: NucleoiconsComponent },
     { path: 'examples/landing',     component: LandingComponent },
     { path: 'examples/login',       component: LoginComponent },
